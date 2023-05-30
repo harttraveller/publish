a7ea4dcbba548800a716c7eac6c8f27cf6f84f99
Hart Traveller <hart@MBP.local>
2023-05-30 03:05:26 -0400

    atlas/atlas/io: create

---
+++ atlas/atlas/io.py
@@ -0,0 +1,9 @@
+"""
+io.py
+- wrap around pyeio to make saving to predefined asset folder very easy, for any given output, probably mostly:
+    - dataframe > .md table
+    - matplotlib > image
+    - plotly > html (for iframe insert)
+"""
+
+# TODO: need to implement
